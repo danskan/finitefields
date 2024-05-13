9583fdf0762a580701a7c07247dac71128c6f89f
Dogecow <87791218+danskan@users.noreply.github.com>
2024-05-13 10:49:40 -0400

    Create ecc.py
    creating classes around field elements

---
+++ ecc.py
@@ -0,0 +1,7 @@
+class FieldElement:
+    def __init__(self, num, prime):
+        if num >= prime or num < 0:
+            error = f'Num {} not in field range 0 to {}'.format(num, prime - 1)
+            raiseValueEError(error)
+        self.num = num
+        self.prime = prime
