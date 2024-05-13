8d5fd075e27d96e7310678c53a4fd3f254054186
Dogecow <87791218+danskan@users.noreply.github.com>
2024-05-13 14:15:10 -0400

    Update ecc.py
    stupid mistakes

--- ecc.py
+++ ecc.py
@@ -1,9 +1,14 @@
 class FieldElement:
     def __init__(self, num, prime):
         if num >= prime or num < 0:
-            error = f'Num {} not in field range 0 to {}'.format(num, prime - 1)
-            raiseValueEError(error)
+            error = 'Num {} not in field range 0 to {}'.format(num, prime - 1)
+            raise ValueError(error)
         self.num = num
         self.prime = prime
     def __repr__(self):
         return 'FieldElement_{}({})'.format(self.prime, self.num)
+    def __eq__(self, other):
+        if other is None:
+            return False   
+        
+
